4bf4a90b3245963c9c53344e92bc27987f50004b
Dilan Livera <Dylan_yahampath@yahoo.com>
2024-01-05 14:35:26 +1100

    add helloworld command

---
+++ YVD/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.CommandLine;
+
+namespace YVD;
+
+internal static class Program
+{
+    private static async Task<int> Main(string[] args)
+    {
+        var helloWorldCommand = new RootCommand(
+            description: "Hello world command build using System.CommandLine");
+
+        var nameOption = new Option<string>(
+            name: "--name",
+            description: "Name of the user.");
+        helloWorldCommand.AddOption(nameOption);
+
+        var cityOption = new Option<string>(
+            name: "--city",
+            description: "City of the user.");
+        helloWorldCommand.AddOption(cityOption);
+
+        helloWorldCommand.SetHandler(
+            (Action<string, string>)HelloWorldHandler,
+            nameOption,
+            cityOption);
+
+        return await helloWorldCommand.InvokeAsync(args);
+    }
+
+    private static void HelloWorldHandler(
+        string name,
+        string city) => Console.WriteLine($"Hello {name} from {city}");
+}
